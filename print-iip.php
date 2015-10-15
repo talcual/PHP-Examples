de5c334d23e893a82df7a5f2fb3def14c3580162
Luis Toscano <admin@caribecoders.com>
2015-10-15 17:15:26 -0500

    Snippet Internet Print Protocol
    Sencillo Script para realizar impresiones sobre CUPS Server

---
+++ print-iip.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+  
+  - - - www.caribecoders.com, www.luistoscano.com - - -
+
+  Example for PHP Printing with PHP::PrintIPP Class
+	easy to use, download de PrintIPP.php class
+	http://www.nongnu.org/phpprintipp/
+
+*/
+
+  
+require_once('php_classes/PrintIPP.php');
+
+try {
+	$ipp = new PrintIPP();                        
+	$ipp->setHost("192.168.40.16");
+	$ipp->setPrinterURI("/printers/HP_LaserJet_Professional_M1212nf_MFP");
+    $ipp->setAttribute('media','A4'); // very little pages
+	$ipp->setAttribute("cpi",18);
+	$ipp->setData("10000030352-CLIENTE.pdf"); // Path to file.
+	$ipp->printJob('laPrueba');   
+} catch (Exception $e) {
+	echo $e;
+}	
+   
+
+?>
